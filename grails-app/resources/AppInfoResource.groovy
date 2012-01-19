c787734674b7e2b40d64a7a74424a348522e6bee
Michael Rice <michael.rice@rackspace.com>
2012-01-18 23:49:03 -0600

    initial commit of simple test app

---
+++ grails-app/resources/AppInfoResource.groovy
@@ -0,0 +1,30 @@
+import javax.ws.rs.GET
+import javax.ws.rs.PUT
+import javax.ws.rs.Path
+import javax.ws.rs.DELETE
+import javax.ws.rs.Consumes
+import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
+
+import javax.ws.rs.core.Response
+
+import static org.grails.jaxrs.response.Responses.*
+
+@Consumes(['application/xml','application/json'])
+@Produces(['application/xml','application/json'])
+
+@Path('/api/hello')
+class AppInfoResource {
+
+    def grailsApplication
+
+    @GET
+    @Path('/world')
+    Response get() {
+       def info = new ArrayList<String>() 
+       info.add("environment": grails.util.Environment.getCurrent().getName())
+       def version = grailsApplication.metadata['app.version']
+       info.add("version": version)
+       ok info 
+    }
+}
